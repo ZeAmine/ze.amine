cbab902e9ee360c596927563bdfc5e1a561a6df8
Amine ZEGMOU <63230216+ZeAmine@users.noreply.github.com>
2021-11-03 16:12:48 +0100

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  RiAccountCircleFill,
+  RiAccountCircleLine,
+  RiHome6Fill,
+  RiHome6Line,
+  RiLayoutMasonryFill,
+  RiLayoutMasonryLine,
+} from "react-icons/all";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [moveSelec, setMoveSelec] = useState({ index: 1 });
+
   return (
     <header className="navbar">
       <nav className="navbar__container">
         <span className="navbar__selector" />
-        <div className="navbar__items">
-          <a href="/" className="navbar__item">
-            <span>Accueil</span>
-          </a>
-          <a href="/" className="navbar__item">
-            <span>Projets</span>
-          </a>
-          <a href="/" className="navbar__item">
-            <span>Contact</span>
-          </a>
-        </div>
+        <ul className="navbar__list">
+          <li className="navbar__item">
+            <a href="/" className="navbar__link">
+              <span>Accueil</span>
+              <RiHome6Line className="navbar__icon" />
+              <RiHome6Fill className="navbar__icon" />
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="/" className="navbar__link">
+              <span>Projets</span>
+              <RiLayoutMasonryLine className="navbar__icon" />
+              <RiLayoutMasonryFill className="navbar__icon" />
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="/" className="navbar__link">
+              <span>Contact</span>
+              <RiAccountCircleLine className="navbar__icon" />
+              <RiAccountCircleFill className="navbar__icon" />
+            </a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
