be893b368370420dfcbb072588bac30f4d06287b
Amine ZEGMOU <63230216+ZeAmine@users.noreply.github.com>
2021-11-28 21:26:34 +0100

    start of animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { RiMailLine } from "react-icons/all";
+import { gsap } from "gsap";
+import { Power4 } from "gsap/gsap-core";
 import "./Hero.css";
 
 const Hero = () => {
+  const textRef = useRef(null);
+  const textRef2 = useRef(null);
+  const textRef3 = useRef(null);
+
+  useEffect(() => {
+    gsap.from([textRef.current, textRef2.current, textRef3.current], {
+      y: 300,
+      duration: 1.3,
+      delay: 1.5,
+    });
+    gsap.to([textRef.current, textRef2.current, textRef3.current], {
+      y: 0,
+      stagger: {
+        each: 0.1,
+      },
+      ease: Power4.easeOut,
+      duration: 1.3,
+      delay: 1.5,
+    });
+  }, []);
+
   return (
     <section className="hero" id="accueil">
       <div className="hero__wrap container">
@@ -17,11 +40,16 @@ const Hero = () => {
         <div className="hero__content">
           <div className="hero_content_container">
             <div className="hero_text_container">
-              <p className="hero__text">
+              <p className="hero__text" ref={textRef}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Maecenas rhoncus viverra ante, id tincidunt turpis placerat
-                vitae suspendisse sollicitudin sodales nulla, in euismod ex
-                mattis sit amet.
+                Maecenas
+              </p>
+              <p className="hero__text" ref={textRef2}>
+                rhoncus viverra ante, id tincidunt turpis placerat vitae
+                suspendisse
+              </p>
+              <p className="hero__text" ref={textRef3}>
+                sollicitudin sodales nulla, in euismod ex mattis sit amet.
               </p>
             </div>
             <div className="hero__contact">
